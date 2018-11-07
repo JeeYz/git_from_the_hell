fe198619558de9dd3b69b32f6c870113714c6330
JeeYz <jkdspiegel@gmail.com>
2018-11-07 13:27:19 +0900

    using multiple arguments in one for loop.

---
+++ for_practice/practice_python_basic_12_multiple_for_loop.py
@@ -0,0 +1,14 @@
+# @Author: JayY
+# @Date:   2018-11-07T13:25:45+09:00
+# @Filename: practice_python_basic_12_multiple_for_loop.py
+# @Last modified by:   JayY
+# @Last modified time: 2018-11-07T13:26:46+09:00
+# @Copyright: JayY
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = [7, 8, 9]
+
+
+for i, j, k in a, b, c:
+    print(i, j, k)
