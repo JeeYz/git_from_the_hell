7526c112fcd8407ae6c62d3edf45c2a0e9e18f33
JeeYz <Jkdspiegel@gmail.com>
2019-03-11 02:18:57 +0900

    change files' name
    change pos name following Korean parsing guide from ETRI.
    This processing is on that guide.

--- Parser/e_44_practicing_input_function.py
+++ Parser/e_44_practicing_input_function.py
@@ -2,7 +2,7 @@
 # @Date:   2019-02-27T15:54:53+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-03-03T23:43:28+09:00
+# @Last modified time: 2019-03-11T02:15:40+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
 
