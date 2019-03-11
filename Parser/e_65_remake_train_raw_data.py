8e259f640a331d22b9aa7b9caef409b80e56d493
JeeYz <jkdspiegel@gmail.com>
2019-03-11 17:57:44 +0900

    dividing train data to several files. remove each one blank lines.

--- Parser/e_65_remake_train_raw_data.py
+++ Parser/e_65_remake_train_raw_data.py
@@ -2,15 +2,12 @@
 # @Date:   2019-03-11T09:39:15+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-03-11T10:30:08+09:00
+# @Last modified time: 2019-03-11T17:34:22+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
 
 filepath = 'd:/Program_Data/'
 filename0 = 'raw_train_dataset_02.train'
-
-filename1 = 'train_dataset_'
-filename2 = '.train'
 
 filename = filepath + filename0
 
@@ -18,12 +15,27 @@ full_data = list()
 with open(filename, 'r', encoding='utf-8') as f:
     one_sent = list()
     switch = 0
+    sent_num = 0
+    file_num = 0
     while True:
         line = f.readline()
         if not line:break
         line = line.split()
         if switch == 1:
             switch = 0
+            sent_num += 1
+            if sent_num%5000 == 0:
+                file_1 = filepath + 'result_train_dataset_%02d.train' %file_num
+                with open(file_1, 'w', encoding='utf-8') as fw:
+                    for i in full_data:
+                        for j in i:
+                            line1 = '\t'.join(j)
+                            fw.write(line1 + '\n')
+                        fw.write('\n')
+                    del full_data
+                    full_data = list()
+                    file_num += 1
+                    print('generating file complete...\n')
             continue
         if line == []:
             full_data.append(one_sent)
@@ -32,7 +44,19 @@ with open(filename, 'r', encoding='utf-8') as f:
             continue
         one_sent.append(line)
 
-print(full_data)
+file_1 = filepath + 'result_train_dataset_%02d.train' %file_num
+with open(file_1, 'w', encoding='utf-8') as fw:
+    for i in full_data:
+        for j in i:
+            line1 = '\t'.join(j)
+            fw.write(line1 + '\n')
+        fw.write('\n')
+    del full_data
+    full_data = list()
+    file_num += 1
+    print('generating file complete...\n')
+
+print('\n ** All files are complete... **')
 
 
 
