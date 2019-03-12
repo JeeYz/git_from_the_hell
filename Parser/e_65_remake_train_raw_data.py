162dfe3908ae7118038b1aca76d3c6fc7fb9f2aa
JeeYz <jkdspiegel@gmail.com>
2019-03-12 15:11:24 +0900

    nothing.

--- Parser/e_65_remake_train_raw_data.py
+++ Parser/e_65_remake_train_raw_data.py
@@ -2,7 +2,7 @@
 # @Date:   2019-03-11T09:39:15+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-03-11T17:34:22+09:00
+# @Last modified time: 2019-03-12T11:33:32+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
 
