6c0214dbfffd9960bfe7519b2cc3e44d2aa68a26
JeeYz <jkdspiegel@gmail.com>
2019-03-13 17:16:27 +0900

    made keras file. start to code in keras.

---
+++ Keras/k_01.py
@@ -0,0 +1,26 @@
+# @Author: J.Y.
+# @Date:   2019-03-13T16:34:16+09:00
+# @Project: NLP
+# @Last modified by:   J.Y.
+# @Last modified time: 2019-03-13T16:38:43+09:00
+# @License: JeeY
+# @Copyright: J.Y. JeeY
+
+import keras
+from keras import models
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## endl
