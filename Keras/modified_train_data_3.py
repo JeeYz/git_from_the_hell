ee426de744d542d965ea0c7c412f1451200c0ad8
JeeYz <jkdspiegel@gmail.com>
2019-03-14 15:56:52 +0900

    modify some error.

--- Keras/modified_train_data_3.py
+++ Keras/modified_train_data_3.py
@@ -2,7 +2,7 @@
 # @Date:   2019-03-14T15:32:13+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-03-14T15:53:47+09:00
+# @Last modified time: 2019-03-14T15:55:21+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
 
