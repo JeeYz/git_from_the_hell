a443703c06580b3626034f304242a64d373ed4e7
JeeYz <jkdspiegel@gmail.com>
2019-03-14 15:12:43 +0900

    1. make a new keras file. 2. make module file of keras01.

--- Keras/keras_01.py
+++ Keras/keras_01.py
@@ -2,15 +2,29 @@
 # @Date:   2019-03-13T16:34:16+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-03-14T09:17:05+09:00
+# @Last modified time: 2019-03-14T12:01:04+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
+
+import sys
+sys.path.append(r'/module')
 
 from keras import models
 from keras import layers
 
+import keras_module_0 as mod0
 
+batch_size = 128
 
+filepath = 'd:/Program_Data/'
+filelist = list()
+
+for i in range(1):
+    filelist.append(filepath + 'result_train_dataset_%02d.train' %i)
+filetuple = tuple(filelist)
+
+for i in filetuple:
+    mod1.generate_train_data(i, batch_size)
 
 
 
---
+++ Keras/module/keras_module_0.py
@@ -0,0 +1,93 @@
+# @Author: J.Y.
+# @Date:   2019-03-14T09:44:24+09:00
+# @Project: NLP
+# @Last modified by:   J.Y.
+# @Last modified time: 2019-03-14T15:10:43+09:00
+# @License: JeeY
+# @Copyright: J.Y. JeeY
+
+import numpy as np
+
+filepath = 'd:/Program_Data/'
+file_word = 'result_raw_words_list_00.words'
+file_pos = 'result_post_temp_01.pos'
+file_arc = 'temp_result_09.temp'
+
+np.random.seed(1)
+wordvec_size = 128
+
+def make_word_list(fname):
+    temp = list()
+    with open(fname, 'r', encoding='utf-8') as f:
+        while True:
+            line = f.readline()
+            if not line:break
+            line = line.split()
+            temp.append(line)
+    words_matrix = np.random.uniform(-1.0, 1.0, (len(temp)+1, wordvec_size))
+    return words_matrix
+
+def make_pos_list(fname):
+    temp = list()
+    with open(fname, 'r', encoding='utf-8') as f:
+        while True:
+            line = f.readline()
+            if not line:break
+            line = line.split()
+            temp.append(line)
+    pos_matrix = np.eye(len(temp)+1)
+    return pos_matrix
+
+def make_arc_list(fname):
+    temp = list()
+    with open(fname, 'r', encoding='utf-8') as f:
+        while True:
+            line = f.readline()
+            if not line:break
+            line = line.split()
+            temp.append(line)
+    arc_matrix = np.eye(len(temp)+1)
+    return arc_matrix
+
+def generate_train_data(filetuple, batch_size):
+    words_matrix = make_word_list(filepath + file_word)
+    pos_matrix = make_pos_list(filepath + file_pos)
+    arc_matrix = make_arc_list(filepath + file_arc)
+
+    train_vector = list()
+    train_label = list()
+
+    for i in filetuple:
+        with open(i, 'r', encoding='utf-8') as f:
+            while True:
+                line = f.readline()
+                if not line:break
+                line = line.split()
+                num1 = 0
+                for j in line[:-1]:
+                    if j == '##':
+                        num1 += 1
+                        continue
+                    if num1 < 18:
+                        pass
+
+                    elif num1 < 36 and 18 <= num1:
+                        pass
+
+                    elif 36 <= num1:
+                        pass
+
+
+    return (train_vector, train_label)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print('hello, world~!')
+    make_word_list(filepath + file_word)
+
+## endl
