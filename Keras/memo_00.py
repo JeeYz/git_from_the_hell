02a28d89062336f63b0fb5db14bfcc7d116617d4
JeeYz <Jkdspiegel@gmail.com>
2019-03-18 08:19:11 +0900

    memo some lines are changed. import keras module.

--- Keras/memo_00.py
+++ Keras/memo_00.py
@@ -2,12 +2,13 @@
 # @Date:   2019-03-14T09:56:12+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-03-15T13:37:23+09:00
+# @Last modified time: 2019-03-17T00:28:36+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
 
 import numpy as np
 import time
+import keras_module_0 as k0
 
 list0 = [1, 2, 3, 4, 5]
 tup0 = tuple(list0)
@@ -21,6 +22,8 @@ print(np.random.uniform(-1.0, 1.0, (3, 4)))
 for i in range(10000):
     print('Number : %d' % i, end='\r')
     # time.sleep(0.1)
+
+
 
 
 
