5534d9a1dc85b8076285dbbc18441d9e6a989c02
JeeYz <jkdspiegel@gmail.com>
2019-03-19 12:12:34 +0900

    generate test data.

--- Keras/keras_01.py
+++ Keras/keras_01.py
@@ -2,7 +2,7 @@
 # @Date:   2019-03-13T16:34:16+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-03-18T17:54:59+09:00
+# @Last modified time: 2019-03-19T11:34:06+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
 
@@ -30,9 +30,11 @@ filelist = list()
 
 ## one dataset for practicing
 filename1 = filepath + 'result_train_dataset_000.train'
-# filename1 = filepath + 'result_train_dataset_000.train'
+filename2 = filepath + 'raw_test_dataset_04.test'
 
 # train_data, train_labels = k0.generate_train_data(filename, batch_size)
+train_data, train_labels = k0.generate_train_data(filename1, batch_size)
+test_data, test_labels = k0.generate_train_data(filename2, batch_size)
 
 network = models.Sequential()
 network.add(layers.Dense(512, activation='relu', input_shape=(input_size, )))
@@ -43,29 +45,15 @@ network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['
 network.summary()
 # print(train_images)
 
-# train_data = train_data.reshape((None, input_size))
-# train_images = train_images.astype('float32')/255
-#
-# test_images = test_images.reshape((10000, 28*28))
-# test_images = test_images.astype('float32')/255
+# train_data, train_labels = k0.generate_train_data(filename1, batch_size)
+# test_data, test_labels = k0.generate_train_data(filename2, batch_size)
 
-# from keras.utils import to_categorical
-
-## making one hot representation
-# train_labels = to_categorical(train_labels)
-# test_labels = to_categorical(test_labels)
-
-train_data, train_labels = k0.generate_train_data(filename, batch_size)
-
-## execution of train data
-# for i in filetuple:
-#     mod1.generate_train_data(i, batch_size)
 network.fit(train_data, train_labels, epochs=epochs, batch_size=batch_size)
 # print(train_images)
 
 ## test session
-# test_loss, test_acc = network.evaluate(test_images, test_labels)
-# print('test_acc: ', test_acc)
+test_loss, test_acc = network.evaluate(test_data, test_labels)
+print('test_acc: ', test_acc)
 
 
 
--- Parser/e_64_change_raw_data_with_number.py
+++ Parser/e_64_change_raw_data_with_number.py
@@ -1,7 +1,7 @@
 # @Author: J.Y.
 # @Date:   2019-03-10T16:47:15+09:00
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-03-18T11:48:48+09:00
+# @Last modified time: 2019-03-19T10:57:54+09:00
 # @License: J.Y. JeeYz
 # @Copyright: J.Y. JeeYz
 
