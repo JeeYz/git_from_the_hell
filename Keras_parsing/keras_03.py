ea3c777fcd274d4a7772edbf84a7f9ff0ad34019
JeeYz <jkdspiegel@gmail.com>
2019-03-21 18:05:10 +0900

    Experiment starts.

--- Keras_parsing/keras_03.py
+++ Keras_parsing/keras_03.py
@@ -1,8 +1,8 @@
 # @Author: J.Y.
-# @Date:   2019-03-13T16:34:16+09:00
+# @Date:   2019-03-21T15:47:00+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-03-21T15:47:35+09:00
+# @Last modified time: 2019-03-21T15:52:20+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
 
@@ -26,13 +26,18 @@ input_size = (18*w_vec_size*2 + 18*p_vec_size*2)
 
 fpath2 = 'd:/Program_Data/Parsing_Data/'
 filelist = list()
-
 for (path, dir, files) in os.walk("d:/Program_Data/Parsing_Data/"):
     for filename in files:
         if '.train' in filename:
             filelist.append(filename)
 filelist = tuple(filelist)
 # print(filelist)
+testlist = list()
+for (path, dir, files) in os.walk("d:/Program_Data/Parsing_Data/"):
+    for filename in files:
+        if '.test' in filename:
+            testlist.append(filename)
+testlist = tuple(testlist)
 
 network = models.Sequential()
 
@@ -41,36 +46,40 @@ network.add(layers.Dense(3, activation='softmax'))
 
 # network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 network.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-
-for i in range(epochs):
-    # for j in filelist:
-    if i != 0:
+network.save_weights('d:/Program_Data/model_weights_k_1.h5', overwrite=True)
+for j in filelist:
+    for i in range(epochs):
         network.load_weights('d:/Program_Data/model_weights_k_1.h5')
         # b = network.get_weights()
         # print('*****************************')
         # print(b)
         # print('*****************************\n\n')
-    # network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-    # filename1 = fpath2 + j
-    filename1 = fpath2 + 'result_train_dataset_000.train'
-    train_data, train_labels = k0.generate_train_data_2(filename1)
-    network.fit(train_data, train_labels, epochs=1, batch_size=batch_size)
-    network.save_weights('d:/Program_Data/model_weights_k_1.h5', overwrite=True)
-    # a = network.get_weights()
-    # print('+++++++++++++++++++++++++++++++++')
-    # print(a)
-    # print('+++++++++++++++++++++++++++++++++\n\n')
+        filename1 = fpath2 + j
+        train_data, train_labels = k0.generate_train_data_2(filename1)
+        network.fit(train_data, train_labels, epochs=10, batch_size=batch_size)
+        network.save_weights('d:/Program_Data/model_weights_k_1.h5', overwrite=True)
+        # a = network.get_weights()
+        # print('+++++++++++++++++++++++++++++++++')
+        # print(a)
+        # print('+++++++++++++++++++++++++++++++++\n\n')
 
 # filename1 = fpath2 + 'result_train_dataset_000.train'
 # train_data, train_labels = k0.generate_train_data_2(filename1)
 # network.fit(train_data, train_labels, epochs=epochs, batch_size=batch_size)
 
 ## test session
-filename2 = fpath2 + 'result_test_dataset_000.test'
-test_data, test_labels = k0.generate_train_data_2(filename2)
-test_loss, test_acc = network.evaluate(test_data, test_labels)
-print('test_acc: ', test_acc)
+# filename2 = fpath2 + 'result_test_dataset_000.test'
+# test_data, test_labels = k0.generate_train_data_2(filename2)
+# test_loss, test_acc = network.evaluate(test_data, test_labels)
+# print('test_acc: ', test_acc)
+total_acc = 0
+for k in testlist:
+    filename2 = fpath2 + k
+    test_data, test_labels = k0.generate_train_data_2(filename2)
+    test_loss, test_acc = network.evaluate(test_data, test_labels)
+    total_acc += test_acc
+print('test_acc: ', test_acc/100)
+
 
 
 
