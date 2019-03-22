7eb2c42f0b239c27d0bd5f397882fb5f08c3d90d
JeeYz <jkdspiegel@gmail.com>
2019-03-22 16:54:25 +0900

    1. experiment. 2. some lines fixed.

--- Keras_parsing/keras_03.py
+++ Keras_parsing/keras_03.py
@@ -2,7 +2,7 @@
 # @Date:   2019-03-21T15:47:00+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-03-21T15:52:20+09:00
+# @Last modified time: 2019-03-22T16:34:32+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
 
@@ -17,68 +17,56 @@ from keras.layers import Input, Dense
 from keras.models import Model, Sequential
 
 import keras_module_0 as k0
+import keras_module_1 as k1
 
 batch_size = 128
-epochs = 10
+epochs = 5
 w_vec_size = 128
 p_vec_size = 73
 input_size = (18*w_vec_size*2 + 18*p_vec_size*2)
+train_limit = 100
+test_limit = 100
 
 fpath2 = 'd:/Program_Data/Parsing_Data/'
-filelist = list()
-for (path, dir, files) in os.walk("d:/Program_Data/Parsing_Data/"):
-    for filename in files:
-        if '.train' in filename:
-            filelist.append(filename)
-filelist = tuple(filelist)
-# print(filelist)
-testlist = list()
-for (path, dir, files) in os.walk("d:/Program_Data/Parsing_Data/"):
-    for filename in files:
-        if '.test' in filename:
-            testlist.append(filename)
-testlist = tuple(testlist)
+
+filelist = k1.generate_file_list(fpath2, '.train')
+testlist = k1.generate_file_list(fpath2, '.test')
 
 network = models.Sequential()
-
 network.add(layers.Dense(512, activation='relu', input_shape=(input_size, )))
 network.add(layers.Dense(3, activation='softmax'))
-
 # network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 network.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 network.save_weights('d:/Program_Data/model_weights_k_1.h5', overwrite=True)
-for j in filelist:
-    for i in range(epochs):
+
+for i in range(epochs):
+    for k,j in enumerate(filelist):
+        # if k == train_limit:
+        #     break
         network.load_weights('d:/Program_Data/model_weights_k_1.h5')
-        # b = network.get_weights()
         # print('*****************************')
-        # print(b)
+        # print(network.get_weights())
         # print('*****************************\n\n')
         filename1 = fpath2 + j
+        print('%d th epoch : ' %(i+1), filename1)
         train_data, train_labels = k0.generate_train_data_2(filename1)
         network.fit(train_data, train_labels, epochs=10, batch_size=batch_size)
         network.save_weights('d:/Program_Data/model_weights_k_1.h5', overwrite=True)
-        # a = network.get_weights()
         # print('+++++++++++++++++++++++++++++++++')
-        # print(a)
+        # print(network.get_weights())
         # print('+++++++++++++++++++++++++++++++++\n\n')
 
-# filename1 = fpath2 + 'result_train_dataset_000.train'
-# train_data, train_labels = k0.generate_train_data_2(filename1)
-# network.fit(train_data, train_labels, epochs=epochs, batch_size=batch_size)
-
 ## test session
-# filename2 = fpath2 + 'result_test_dataset_000.test'
-# test_data, test_labels = k0.generate_train_data_2(filename2)
-# test_loss, test_acc = network.evaluate(test_data, test_labels)
-# print('test_acc: ', test_acc)
 total_acc = 0
-for k in testlist:
+for j,k in enumerate(testlist):
+    # if j == test_limit:
+    #     break
     filename2 = fpath2 + k
+    print(filename2)
     test_data, test_labels = k0.generate_train_data_2(filename2)
     test_loss, test_acc = network.evaluate(test_data, test_labels)
     total_acc += test_acc
-print('test_acc: ', test_acc/100)
+print('test_acc: ', total_acc/test_limit)
 
 
 
---
+++ Keras_parsing/module/keras_module_1.py
@@ -0,0 +1,30 @@
+# @Author: J.Y.
+# @Date:   2019-03-22T09:55:52+09:00
+# @Project: NLP
+# @Last modified by:   J.Y.
+# @Last modified time: 2019-03-22T10:30:44+09:00
+# @License: JeeY
+# @Copyright: J.Y. JeeY
+
+import os
+
+def generate_file_list(path, word_for_target):
+    filelist = list()
+    for (path, dir, files) in os.walk(path):
+        for filename in files:
+            if word_for_target in filename:
+                filelist.append(filename)
+    filelist = tuple(filelist)
+    return filelist
+
+
+
+
+
+
+if __name__ == "__main__":
+    print('hello, world~!!')
+
+
+
+## endl
