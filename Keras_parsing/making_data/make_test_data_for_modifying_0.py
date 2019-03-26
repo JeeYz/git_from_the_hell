d7ba72e2e9eb1740a5707e3229101ce8413de1a2
JeeYz <jkdspiegel@gmail.com>
2019-03-26 11:52:53 +0900

    make new test file. except useless lines. remain only first line of each sentence.

--- Keras_parsing/making_data/make_test_data_for_modifying_0.py
+++ Keras_parsing/making_data/make_test_data_for_modifying_0.py
@@ -2,12 +2,28 @@
 # @Date:   2019-03-26T11:36:01+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-03-26T11:38:16+09:00
+# @Last modified time: 2019-03-26T11:52:06+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
 
 fpath1 = 'd:/Program_Data/'
-filewrite = 'raw_test_dataset_04.test'
+filename1 = fpath1 + 'raw_test_dataset_04.test'
+filename2 = fpath1 + 'raw_test_dataset_05.test'
+
+with open(filename1, 'r', encoding='utf-8') as f1, open(filename2, 'a', encoding='utf-8') as f2:
+    switch = 1
+    while True:
+        line = f1.readline()
+        if not line:break
+        line = line.split()
+        if switch == 1:
+            f2.write('\t'.join(line) + '\n\n')
+            switch = 0
+            continue
+        if line == []:
+            switch = 1
+            continue
+
 
 
 
