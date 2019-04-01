377b4fa16292d6a4787d560dc64ea5bbe068ea7c
JeeYz <jkdspiegel@gmail.com>
2019-04-01 18:00:00 +0900

    mid backup

--- Keras_parsing/keras_06_practice_embedding_function.py
+++ Keras_parsing/keras_06_practice_embedding_function.py
@@ -2,7 +2,7 @@
 # @Date:   2019-03-28T11:03:33+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-04-01T16:05:09+09:00
+# @Last modified time: 2019-04-01T17:51:47+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
 
@@ -56,7 +56,8 @@ embedding_layer2 = Embedding(P_VEC_SIZE,
 input_layer = Input(shape=(INPUT_SIZE, ), dtype = 'int32')
 sequence_input1 = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
 sequence_input2 = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
-embedded_sequences = embedding_layer(sequence_input1) + embedding_layer(sequence_input2)
+# embedded_sequences = embedding_layer1(sequence_input1) + embedding_layer2(sequence_input2)
+embedded_sequences = layers.concatenate([sequence_input1, sequence_input2], axis=-1)
 # network.add(layers.Dense(512, activation='relu', input_shape=(INPUT_SIZE, )))
 # network.add(layers.Dense(3, activation='softmax'))
 x = layers.Dense(512, activation='relu')(embedded_sequences)
@@ -77,7 +78,7 @@ for i in range(EPOCHS):
         # print('*****************************\n\n')
         filename1 = fpath2 + j
         print('%d th epoch : ' %(i+1), filename1)
-        train_data, train_labels = k0.generate_train_data_2(filename1)
+        train_data, train_labels = k3.generate_train_data_3(filename1)
         network.fit(train_data, train_labels, epochs=15, batch_size=BATCH_SIZE)
         network.save_weights('d:/Program_Data/model_weights_k_3.h5', overwrite=True)
         # print('+++++++++++++++++++++++++++++++++')
--- Keras_parsing/module/keras_module_3.py
+++ Keras_parsing/module/keras_module_3.py
@@ -2,7 +2,7 @@
 # @Date:   2019-03-28T11:14:41+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-04-01T11:55:30+09:00
+# @Last modified time: 2019-04-01T17:59:52+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
 
@@ -37,7 +37,37 @@ def make_pos_list():
     pos_matrix = np.eye(len(temp)+1)
     return pos_matrix
 
+def generate_train_data3(fname):
+    full_train_vectors = list()
+    train_label = list()
+    with open(fname, 'r', encoding='utf-8') as f:
+        while True:
+            word_vectors = list()
+            pos_vectors = list()
+            line = f.readline()
+            if not line: break
+            line = line.split()
+            if line == []: continue
+            num1 = 0
+            for j in line[:-1]:
+                if j == '##':
+                    num1 += 1
+                    continue
+                if num1 < 18:
+                    word_vectors.extend(words_matrix[int(j)])
+                elif num1 < 36 and 18 <= num1:
+                    train_vector.extend(pos_matrix[int(j)])
+            full_train_vectors.append(train_vector)
 
+            if int(line[-1]) == 0:
+                train_label.append([1, 0, 0])
+            elif int(line[-1]) == 1:
+                train_label.append([0, 1, 0])
+            elif int(line[-1]) == 2:
+                train_label.append([0, 0, 1])
+            num1 += 1
+
+    return train_data, train_labels
 
 
 
