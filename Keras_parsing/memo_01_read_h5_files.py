25e3227a3c12ba5ea9fdce76a0d86321fd7da9a1
JeeYz <Jkdspiegel@gmail.com>
2019-04-08 09:06:03 +0900

    home backup 2. memo_01_read_h5_files.py

--- Keras_parsing/memo_01_read_h5_files.py
+++ Keras_parsing/memo_01_read_h5_files.py
@@ -2,10 +2,11 @@
 # @Date:   2019-04-04T14:30:17+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-04-08T07:08:42+09:00
+# @Last modified time: 2019-04-08T08:56:39+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
 
+import numpy as np
 import h5py
 filename = 'd:/Program_Data/model_weights_k_3.h5'
 f = h5py.File(filename, 'r')
@@ -16,8 +17,14 @@ a_group_key = list(f.keys())[0]
 
 # Get the data
 data = list(f[a_group_key])
+data_set = f['embedding_1']
+print(data_set)
 
-print(f['embedding_1'])
+print(['dense_1'])
+
+b = f.get('dense_1')
+print(b)
+
 
 
 ## endl
