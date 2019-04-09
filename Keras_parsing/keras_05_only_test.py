a135f1b4170fa2eb09b8ab42fd71f136997a2357
JeeYz <jkdspiegel@gmail.com>
2019-04-09 09:32:03 +0900

    for merge.

--- Keras_parsing/keras_05_only_test.py
+++ Keras_parsing/keras_05_only_test.py
@@ -2,7 +2,7 @@
 # @Date:   2019-03-21T15:47:00+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-04-08T18:04:35+09:00
+# @Last modified time: 2019-04-09T09:31:26+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
 
