dbf2d14428a0473f860d79e3d357b51b100858ce
JeeYz <jkdspiegel@gmail.com>
2019-04-11 17:06:00 +0900

    mid backup. lab working was pulled to github.

--- Keras_parsing/making_data_2/making_train_data_1.py
+++ Keras_parsing/making_data_2/making_train_data_1.py
@@ -2,9 +2,11 @@
 # @Date:   2019-04-11T11:06:59+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-04-11T15:00:15+09:00
+# @Last modified time: 2019-04-11T17:05:54+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
+
+import time
 
 fname0 = 'd:/Program_Data/raw_train_dataset_07.train'
 fname1 = 'd:/Program_Data/raw_train_dataset_08.train'
@@ -26,33 +28,53 @@ with open(fname2, 'r', encoding='utf-8') as f:
             full_sent_data.append(one_sent)
             continue
         if switch == 1:
-            switch == 0
+            switch = 0
             one_sent = list()
             continue
         one = list()
         one.append(line[3])
         one.append(line[5])
-
-with open(fname0, 'r', encoding='utf-8') as fr, open(fname1, 'a', encoding='utf-8') as fw,\
-open(fname2, 'r', encoding='utf-8') as fr2:
+        one_sent.append(one)
+# print(full_sent_data)
+with open(fname0, 'r', encoding='utf-8') as fr, open(fname1, 'a', encoding='utf-8') as fw:
     switch = 1
     for i,j in enumerate(full_sent_data):
-        action_stack = list()
-        stack, buffer = m0.return_stack_buffer(j)
+        if i == 1:
+            print('P A U S E ! !')
+            time.sleep(10000)
+        stack, buffer, action_stack = m0.return_stack_buffer_act_stack(j)
+        # print(j)
+        num = 0
         while True:
-            line = fr1.readline()
+            line = fr.readline()
             if not line: break
             line = line.split()
+
+            print(' *** %d ***' %num)
+            print('<< stack >>')
+            print(stack, '\n')
+            print('<< buffer >>')
+            print(buffer, '\n')
+
             if line == []:
                 fw.write('\n')
                 switch = 1
+                num = 0
                 break
             if switch == 1:
-                new_line, action_stack = m0.generate_one_train_data(line, switch, action_stack)
-                switch == 0
-                continue
-            new_line, action_stack = m0.generate_one_train_data(line, switch, action_stack)
-
+                new_line, stack, buffer, action_stack = m0.generate_one_train_data(line, switch,
+                                                                            stack, buffer, action_stack)
+                print('<< new_line >>')
+                print(new_line, '\n')
+                fw.write('   '.join(new_line) + '\n')
+                switch = 0
+            else:
+                new_line, stack, buffer, action_stack = m0.generate_one_train_data(line, switch,
+                                                                            stack, buffer, action_stack)
+                print('<< new_line >>')
+                print(new_line, '\n')
+                fw.write('   '.join(new_line) + '\n')
+            num += 1
 
 
 
--- Keras_parsing/making_data_2/module_0.py
+++ Keras_parsing/making_data_2/module_0.py
@@ -2,9 +2,11 @@
 # @Date:   2019-04-11T11:14:06+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-04-11T14:56:08+09:00
+# @Last modified time: 2019-04-11T17:02:01+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
+
+import copy
 
 class tree_node:
     def __init__(self, i, w):
@@ -30,24 +32,146 @@ def make_small_dict():
             small_dict[line[5]] = line[7]
     return small_dict
 
-def return_stack_buffer(j):
-    
-    return stack, buffer
+def return_stack_buffer_act_stack(j):
+    action_stack = list()
+    stack = list()
+    buffer = copy.deepcopy(j)
+    stack.append(['ROOT', 'ROOT'])
+    new = tree_node(int(0), ['ROOT', 'ROOT'])
+    for m,n in enumerate(j):
+        new = tree_node(int(m), n)
+        action_stack.append(new)
+    return stack, buffer, action_stack
 
-def generate_depend_list(d_list):
-    return d_list
+def find_children_1(one, action_stack):
+    temp = list()
+    if one == ['ROOT', 'ROOT'] or one == ['NULL', 'NULL']:
+        for k in range(4):
+            temp.extend(['NULL', 'NULL', '##'])
+    else:
+        for node in action_stack:
+            if node.word == one:
+                if node.children == []:
+                    for k in range(4):
+                        temp.extend(['NULL', 'NULL', '##'])
+                else:
+                    if len(node.children) == 1:
+                        temp.extend(node.children[0])
+                        temp.append('##')
+                        temp.extend(node.children[0])
+                        temp.append('##')
+                        temp.extend(['NULL', 'NULL', '##'])
+                        temp.extend(['NULL', 'NULL', '##'])
+                    else:
+                        temp.append(node.children[0])
+                        temp.append('##')
+                        temp.append(node.children[-1])
+                        temp.append('##')
+                        temp.append(node.children[1])
+                        temp.append('##')
+                        temp.append(node.children[-2])
+                        temp.append('##')
+    return temp
 
-def generate_one_train_data(line, switch, action_stack):
+def find_children_2(one, action_stack):
+    temp = list()
+    if one == ['ROOT', 'ROOT'] or one == ['NULL', 'NULL']:
+        for k in range(2):
+            temp.extend(['NULL', 'NULL', '##'])
+    else:
+        for node in action_stack:
+            if stack[i] == node.word:
+                if node.children == []:
+                    for k in range(2):
+                        temp.extend(['NULL', 'NULL', '##'])
+                else:
+                    if len(node.children) == 1:
+                        for m in action_stack:
+                            if m.word == node.children[0]:
+                                if m.children == []:
+                                    for k in range(2):
+                                        temp.extend(['NULL', 'NULL', '##'])
+                                else:
+                                    if len(m.children) == 1:
+                                        temp.extend(m.children[0])
+                                        temp.append('##')
+                                        temp.extend(m.children[0])
+                                        temp.append('##')
+                                    else:
+                                        temp.extend(m.children[0])
+                                        temp.append('##')
+                                        temp.extend(m.children[-1])
+                                        temp.append('##')
+                    else:
+                        for m in action_stack:
+                            if m.word == node.children[0]:
+                                if m.children == []:
+                                    for k in range(2):
+                                        temp.extend(['NULL', 'NULL', '##'])
+                                else:
+                                    if len(m.children) == 1:
+                                        temp.extend(m.children[0])
+                                        temp.append('##')
+                                    else:
+                                        temp.extend(m.children[0])
+                                        temp.append('##')
+                            if m.word == node.children[-1]:
+                                if m.children == []:
+                                    for k in range(2):
+                                        temp.extend(['NULL', 'NULL', '##'])
+                                else:
+                                    if len(m.children) == 1:
+                                        temp.extend(m.children[-1])
+                                        temp.append('##')
+                                    else:
+                                        temp.extend(m.children[-1])
+                                        temp.append('##')
+    return temp
+
+def generate_one_train_data(line, switch, stack, buffer, action_stack):
+    temp = list()
     new_line = list()
-    if siwtch == 1:
+    if switch == 1:
         new_line.extend(line[:-1])
         for i in range(12):
             new_line.extend(['NULL', 'NULL', '##'])
         new_line.append(line[-1])
     else:
-        pass
+        new_line.extend(line[:-1])
+        one = list()
+        for j in range(6):
+            if j == '##':
+                child = find_children_1(one, action_stack)
+                new_line.extend(child)
+                one = list()
+            else:
+                one.append(j)
+        one = list()
+        for j in range(6):
+            if j == '##':
+                child = find_children_2(one, action_stack)
+                new_line.extend(child)
+                one = list()
+            else:
+                one.append(j)
+        new_line.append(line[-1])
 
-    return new_line
+    if line[-1] == 'shift':
+        stack.insert(0, buffer[0])
+        del buffer[0]
+    elif line[-1] == 'left':
+        temp_depend = list()
+        for node in action_stack:
+            if node.word == stack[0]:
+                node.children.append(stack[1])
+            elif node.word == stack[1]:
+                temp_depend = stack[0]
+        for node in action_stack:
+            if node.depend == temp_depend:
+                node.depend = int(node.index)
+        del stack[1]
+
+    return new_line, stack, buffer, action_stack
 
 
 
