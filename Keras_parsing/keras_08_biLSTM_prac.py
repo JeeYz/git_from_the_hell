93c7dc8705b56ef8129b725e967238b45e49c92b
JeeYz <jkdspiegel@gmail.com>
2019-04-25 10:57:32 +0900

    1. make new file. for practicing biLSTM

---
+++ Keras_parsing/keras_08_biLSTM_prac.py
@@ -0,0 +1,27 @@
+# @Author: J.Y.
+# @Date:   2019-04-25T10:56:41+09:00
+# @Project: NLP
+# @Last modified by:   J.Y.
+# @Last modified time: 2019-04-25T10:56:51+09:00
+# @License: JeeY
+# @Copyright: J.Y. JeeY
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## endl
