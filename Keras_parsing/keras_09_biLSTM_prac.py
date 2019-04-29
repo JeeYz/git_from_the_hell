44e8c8420b167d93dac359b87523abd6e3975be5
JeeYz <jkdspiegel@gmail.com>
2019-04-29 09:44:20 +0900

    merge home and lab.

--- Keras_parsing/keras_09_biLSTM_prac.py
+++ Keras_parsing/keras_09_biLSTM_prac.py
@@ -2,7 +2,7 @@
 # @Date:   2019-04-25T10:56:41+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-04-26T15:35:38+09:00
+# @Last modified time: 2019-04-29T09:43:33+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
 
--- Keras_parsing/module/parsing_module_2.py
+++ Keras_parsing/module/parsing_module_2.py
@@ -2,7 +2,7 @@
 # @Date:   2019-04-25T11:43:51+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-04-25T11:45:58+09:00
+# @Last modified time: 2019-04-29T09:43:27+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
 
