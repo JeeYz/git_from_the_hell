f1ed979e0bc9401e3033af4b07b35e59b84e332b
JeeYz <Jkdspiegel@gmail.com>
2019-05-07 08:36:14 +0900

    home working.

--- Keras_parsing/keras_09_biLSTM_prac.py
+++ Keras_parsing/keras_09_biLSTM_prac.py
@@ -2,8 +2,8 @@
 # @Date:   2019-04-25T10:56:41+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-05-07T03:16:41+09:00
-# @Last modified time: 2019-05-07T03:16:41+09:00
+# @Last modified time: 2019-05-07T03:37:31+09:00
+# @Last modified time: 2019-05-07T03:37:31+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
 
