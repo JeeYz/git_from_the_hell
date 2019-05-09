05818888f236cb714fa2ca2899b618f7d7fbb551
JeeYz <jkdspiegel@gmail.com>
2019-05-09 17:11:47 +0900

    lab backup.

--- Keras_parsing/keras_10_biLSTM_prac_one_example.py
+++ Keras_parsing/keras_10_biLSTM_prac_one_example.py
@@ -2,7 +2,7 @@
 # @Date:   2019-05-09T11:28:21+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-05-09T11:51:30+09:00
+# @Last modified time: 2019-05-09T14:07:19+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
 
@@ -23,15 +23,9 @@ from keras.models import Model, Sequential
 from keras import Input
 from keras.initializers import Constant
 
-import keras_module_0 as k0
-import keras_module_1 as k1
-import keras_module_2 as k2
-import keras_module_3 as k3
 import keras_module_for_fastText as kfT
 
-import parsing_module_0 as p0
-import parsing_module_1 as p1
-import parsing_module_2 as p2
+import parsing_module_3 as p3
 
 BATCH_SIZE = 128
 EPOCHS = 10
@@ -41,53 +35,42 @@ INPUT_SIZE = (18*W_VEC_SIZE*2 + 18*P_VEC_SIZE*2)
 
 fpath2 = 'd:/Program_Data/Parsing_Data/'
 filewrite = '00_result_training.result'
-trainfile = 'd:/Program_Data/raw_train_dataset_21.train'
+trainfile = 'd:/Program_Data/raw_train_dataset_23.train'
 savepara_name = 'd:/Program_Data/model_weights_k_17_bi_LSTM_proto.h5'
 
-# words_matrix = k3.make_word_list(W_VEC_SIZE)
-# pos_matrix = k3.make_pos_list(P_VEC_SIZE)
 words_matrix = kfT.words_matrix_fastText(W_VEC_SIZE)
 pos_matrix = kfT.make_pos_fastText(P_VEC_SIZE)
 
-all_word, all_pos = p2.make_all_data()
-all_label, all_sent = p2.make_label_matrix()
-# all_init_test = p0.make_all_init_test_data()
-# w_dict, p_dict = p0.make_words_pos_dict()
+word, pos, label = p3.make_small_train_data()
 
 w = Input(shape=(None, 2), dtype='int32', name='words')
 p = Input(shape=(None, 2), dtype='int32', name='pos')
+print(w, p)
 
 embedding_layer1 = Embedding(len(words_matrix), W_VEC_SIZE,
                             embeddings_initializer=Constant(words_matrix))
 embedding_layer2 = Embedding(len(pos_matrix), P_VEC_SIZE,
                             embeddings_initializer=Constant(pos_matrix))
 
-print(w, p)
 ew1 = embedding_layer1(w)
 ep1 = embedding_layer2(p)
+length = len(w[0][1])
+print(length)
 print(ew1, ep1)
 ew1 = Flatten()(ew1)
 ep1 = Flatten()(ep1)
 print(ew1, ep1)
 es = layers.concatenate([ew1, ep1], axis=-1) ## es = embedded_sequences
 print(es)
-# es = Flatten()(es) ## es = embedded_sequences
-# es = [es, es]
-# print(es)
 
 x = Bidirectional(LSTM(128, return_sequences=True,
-                    input_shape=(1, , 512)), merge_mode='concat')(es)
+                    input_shape=(1, length, 512)), merge_mode='concat')(es)
 
-# x = Bidirectional(LSTM(length, return_sequences=True,
-#                     dropout=0.15, recurrent_dropout=0.15,
-#                     input_shape=(1, 512), BATCH_SIZE=1))(embedded_sequences)
 print(x)
 x = Dropout(0.4)(x)
 a = layers.Dense(512, activation='relu')(x)
 b = layers.Dense(512, activation='relu')(x)
 
-# a = p2.make_matrix_A(a)
-# b = p2.make_matrix_A(b)
 b = backend.permute_dimensions(b, (0, 2, 1))
 
 x = layers.Dense((W_VEC_SIZE+1)*W_VEC_SIZE)(a)
@@ -105,12 +88,10 @@ correct = 0
 total_q = 0
 num = 0
 for i in range(EPOCHS):
-    for k,sent_words in enumerate(all_word):
-        network.load_weights(savepara_name)
-        network.fit({'words':sent_words, 'pos':all_pos[k],
-                        'length':len(all_sent[k])}, all_label[k],
-                        epochs=5, batch_size=1)
-        network.save_weights(savepara_name, overwrite=True)
+    network.load_weights(savepara_name)
+    network.fit({'words':word, 'pos':pos}, label,
+                    epochs=5, batch_size=1)
+    network.save_weights(savepara_name, overwrite=True)
 
 
 
--- Keras_parsing/module/parsing_module_3.py
+++ Keras_parsing/module/parsing_module_3.py
@@ -2,13 +2,36 @@
 # @Date:   2019-05-09T11:29:24+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-05-09T11:29:30+09:00
+# @Last modified time: 2019-05-09T13:26:56+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
 
+trainfile = 'd:/Program_Data/raw_train_dataset_23.train'
 
-
-
+def make_small_train_data():
+    word = list()
+    pos = list()
+    label = list()
+    sent_w = list()
+    sent_p = list()
+    with open(trainfile, 'r', encoding='utf-8') as f:
+        switch = 1
+        while True:
+            line = f.readline()
+            if not line:break
+            line = line.split()
+            if line == []:
+                switch = 2
+                continue
+            if switch == 2:
+                label.append([line[0], line[1]])
+                continue
+            word.append([line[1], line[2]])
+            pos.append([line[3], line[4]])
+    print(word)
+    print(pos)
+    print(label)
+    return [word], [pos], label
 
 
 
