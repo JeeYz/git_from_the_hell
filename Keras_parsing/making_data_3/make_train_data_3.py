2971c89218247901f8c5ceeb8a9afaee140eed7a
JeeYz <jkdspiegel@gmail.com>
2019-05-21 17:24:12 +0900

    backup.

--- Keras_parsing/making_data_3/make_train_data_3.py
+++ Keras_parsing/making_data_3/make_train_data_3.py
@@ -2,13 +2,14 @@
 # @Date:   2019-05-09T11:30:05+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-05-20T16:19:09+09:00
+# @Last modified time: 2019-05-21T16:33:28+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
 
 trainfile = 'd:/Program_Data/raw_train_dataset_22.train'
 trainfile1 = 'd:/Program_Data/raw_train_dataset_05.train'
 trainfile2 = 'd:/Program_Data/raw_train_dataset_23.train'
+trainfile3 = 'd:/Program_Data/raw_train_dataset_24.train'
 
 with open(trainfile, 'r', encoding='utf-8') as fr, open(trainfile2, 'a', encoding='utf-8') as fw:
     while True:
