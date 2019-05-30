0d3cb21b8c404b36b513e5da9d012b5ff6d88542
JeeYz <jkdspiegel@gmail.com>
2019-05-30 14:54:42 +0900

    make CYK algorithm.

---
+++ Keras_parsing/memo_03.py
@@ -0,0 +1,24 @@
+# @Author: J.Y.
+# @Date:   2019-05-30T10:22:12+09:00
+# @Project: NLP
+# @Last modified by:   J.Y.
+# @Last modified time: 2019-05-30T14:45:38+09:00
+# @License: JeeY
+# @Copyright: J.Y. JeeY
+
+import numpy as np
+
+# for i in reversed(range(10)):
+#     print(i)
+
+# for j in range(1, 10):
+#     print(j)
+
+for k in reversed(range(4, 10)):
+    print(k)
+
+
+
+
+
+## endl
--- Keras_parsing/module/cyk_algorithm.py
+++ Keras_parsing/module/cyk_algorithm.py
@@ -2,32 +2,51 @@
 # @Date:   2019-05-27T10:29:01+09:00
 # @Project: NLP
 # @Last modified by:   J.Y.
-# @Last modified time: 2019-05-29T16:35:06+09:00
+# @Last modified time: 2019-05-30T14:54:27+09:00
 # @License: JeeY
 # @Copyright: J.Y. JeeY
 
-def CYK_algorithm_func():
-    for lg in range(2, len()):
-        pass
-    return
+import numpy as np
+import time
+import copy
 
 
-
-
-class CYK_table():
+class CYK_table(length, result_matrix):
     def __init__(self):
+        self.sent_length = length
+        self.main()
+
+    def main(self):
+        table = make_cyk_table()
+        for i in range(1, len(self.sent_length)):
+            for j in reversed(range(i, len(self.sent_length)):
+                for k in range(1, i):
+                    generate_nodes()
+                    pass
+
+    def generate_nodes(self):
+        pass
+
+    def make_cyk_table(self):
+        table = list()
+        table = np.zeors((self.sent_length, self.sent_length))
+        return table
+
+    def select_max_value(self):
+
         pass
 
 
-
-class CYK_algorithm():
+class CYK_one_node():
     def __init__(self):
-        self.result = list()
-
+        self.first_score = list()
+        self.second_score = list()
+        self.arc_score = list()
 
     def main(self):
 
         pass
+
 
 
 
