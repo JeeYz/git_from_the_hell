ddf03d909f9585c3f8d2cdf70a3289c262b5f6ef
JeeYz <jkdspiegel@gmail.com>
2019-09-06 18:45:00 +0900

    add memo no.6

---
+++ Keras_parsing/memo6.py
@@ -0,0 +1,25 @@
+# @Author: J.Y.
+# @Date:   2019-09-06T18:41:11+09:00
+# @Project: NLP
+# @Last modified by:   J.Y.
+# @Last modified time: 2019-09-06T18:41:42+09:00
+# @License: JeeY
+# @Copyright: J.Y. JeeY
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## endl
